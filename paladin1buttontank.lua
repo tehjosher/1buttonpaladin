f858149f6d3de98618b767173b2377d0cf115f28
j0rsche <78011741+tehjosher@users.noreply.github.com>
2025-01-29 10:07:17 -0500

    Update paladin1buttontank.lua
    updated for current patch, general tanking use applying wisdom yourself 1/29/2025

--- paladin1buttontank.lua
+++ paladin1buttontank.lua
@@ -1,21 +1,23 @@
-function paladin_1buttontank()
+function pallytank()
     -- spell names
-    local holy_strike = "Holy Strike"
     local seal_of_wisdom = "Seal of Wisdom"
     local judgement = "Judgement"
     local judgement_of_wisdom = "Judgement of Wisdom"
-    local crusader_strike = "Crusader Strike"
-    local seal_of_righteousness = "Seal of Righteousness"
-    local exorcism = "Exorcism"
     local holy_shield = "Holy Shield"
+    local holy_strike = "Holy Strike"
+    local right = "Seal of Righteousness"
+    local zeal = "Zeal"
+    local crusader = "Crusader Strike"
 
-    -- Keep up Holy Shield
-    if not buffed(holy_shield) and cooldown_for(holy shield) == 0 then
+
+    if not buffed(holy_shield) then
         cast(holy_shield)
-        return
     end
 
-    -- Holy Strike on cooldown.
+    if not buffed(zeal) and cooldown_for(crusader) == 0 then
+        cast_and_downrank(crusader)
+    end
+
     if cooldown_for(holy_strike) == 0 then
         cast_and_downrank(holy_strike)
     end
@@ -31,40 +33,14 @@ function paladin_1buttontank()
         return
     end
 
-    -- Keep up Seal of Righteousness
-    if not buffed(seal_of_righteousness) then
-        cast(seal_of_righteousness)
+    -- Keep up Right when Wisdom up
+    if not buffed(right) and cooldown_for(right) == 0 then
+       cast(right)
         return
     end
 
-    -- Apply the first stack of Crusader Strike.
-    -- TODO: write a function to get the number of stacks
-    if not buffed(crusader_strike, "target") and cooldown_for(crusader_strike) == 0 then
-        cast(crusader_strike)
-        return
-    end
-
-    -- Cast Exorcism if the target is an undead or demon, and we have 30% or more mana.
-    local mana = UnitMana("player")
-    local max_mana = UnitManaMax("player")
-    
-    local target_type = UnitCreatureType("target")
-    if ((target_type == "Undead") or (target_type == "Demon")) and ((mana/max_mana) > .3) and (cooldown_for(exorcism) == 0) then
-        cast(exorcism)
-        return
-    end
-
-    -- Judge Seal of Righteousness on cooldown if we are above 20% mana.
-
-    if (mana/max_mana) > 0.2 and cooldown_for(judgement) == 0 then
-        cast(judgement)
-        return
-    end
-
-    -- Finally, spam Crusader Strike.
-    -- The rank only really matters on the first application.
-    if cooldown_for(crusader_strike) == 0 then
-        cast_and_downrank(crusader_strike)
+    if buffed(right) and cooldown_for(judgement) == 0 then
+      cast(judgement)
     end
 end
 
@@ -89,15 +65,4 @@ function spell_index(spell_name, spell_rank)
 
         i = i + 1
     end
-end
-
--- If under 20% mana, cast the Rank 1 version of a spell.
-function cast_and_downrank(spell_name)
-    local mana = UnitMana("player")
-    local max_mana = UnitManaMax("player")
-    if mana/max_mana < 0.2 then
-        spell_name = string.format("%s(Rank 1)", spell_name)
-    end
-
-    cast(spell_name)
 end
